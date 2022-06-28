bfd33fb2cdfc722cdd745acde208903259207173
HE Yaowen <he.yaowen@hotmail.com>
2022-06-28 19:53:00 +0800

    Update post hook to generate files

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,10 +1,22 @@
 import os
+import shutil
 from glob import glob
 
 license_id = '{{ cookiecutter.license_id }}'
+use_vuex = '{{ cookiecutter.use_vuex }}'
+use_docker = '{{ cookiecutter.use_docker }}'
 
 if license_id != 'None':
     os.rename('LICENSE.{{ cookiecutter.license_id }}', 'LICENSE')
 
 for license_file in glob('LICENSE.*'):
     os.unlink(license_file)
+
+if use_vuex == 'no':
+    shutil.rmtree('src/store')
+
+if use_docker == 'no':
+    shutil.rmtree('docker')
+    os.unlink('Dockerfile')
+    os.unlink('docker-compose.yml')
+    os.unlink('.dockerignore')
